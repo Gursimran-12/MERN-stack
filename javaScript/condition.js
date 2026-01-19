57857422e013e9c3c8002da95c586cfeac8aed31
Gursimran Kaur <gursim326@gmail.com>
2026-01-19 23:14:11 +0530

    Conditional Statements

--- javaScript/condition.js
+++ javaScript/condition.js
@@ -33,6 +33,7 @@ else {
 }
 */
 
+/*
 // Authentication using the conditional statements
 
 // Let us assume, following data shows the stored username and password of the user.
@@ -53,4 +54,30 @@ if(username == user && password == passcode) { // Checking both username and pas
 else {
     alert(`Invalid username or password`); // Alert, if we enter wrong username and password 
 }
+*/
 
+/*
+// Even and odd Exercise
+
+let num = parseInt(prompt("Enter the number")); // getting input from user
+
+// Checking if the number is even or odd
+
+if(num%2 == 0){
+    console.log(`${num} is an even number`);
+}
+else {
+    console.log(`${num} is an odd number`);
+}
+
+*/
+
+// Weekend checker
+
+let day = prompt("Enter the day") // getting value from the user
+if(day === "Saturday" || day === "Sunday"){
+    console.log("It's a weekend day");
+}
+else {
+    console.log("It is not a weekend day");
+}
