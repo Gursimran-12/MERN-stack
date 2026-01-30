77598d9f4c65c7cc6d6c7ef2662e84634a4efeb7
Gursimran Kaur <gursim326@gmail.com>
2026-01-30 16:31:38 +0530

    Form Validation using events

--- events/tasks/formValidation/javaScript/script.js
+++ events/tasks/formValidation/javaScript/script.js
@@ -1,16 +1,29 @@
-function validateForm() {
-    const username = document.getElementById("username").value;
-    const age = document.getElementById("age").value;
-    const emailId = document.getElementById("emailId").value;
-    const uPassword = document.getElementById("uPassword").value;
-    const error = document.getElementsByClassName("error");
+const form = document.getElementById("myForm");
+const emailId = document.getElementById("emailId");
+const emailError = document.getElementById("emailError");
+const uPassword = document.getElementById("uPassword");
+const passError = document.getElementById("passError");
+const result = document.getElementById("result");
 
-    if (username == " ") {
-        console.log("Name field is required");
-        return false
+form.addEventListener("submit", function (event) {
+    if (emailId.value.trim() === "") {
+        emailError.style.display = "block";
+        event.preventDefault();
     }
+    else if (uPassword.value.trim() === "") {
+        passError.style.display = "block";
+        event.preventDefault();
+    }
+    else {
+        emailError.innerText = "";
+        result.style.display = "block";
+        event.preventDefault();
+        
+    }
+});
 
-    return true;
-}
+
+
+
 
 
